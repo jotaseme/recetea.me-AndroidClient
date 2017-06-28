2ded3583eff71a3abf2c12f68ec083f31aeae512
Jose Maria <jotaememoreno@gmail.com>
2017-06-28 16:33:11 +0200

    saveUserToken method added to class

--- app/src/main/java/es/upm/miw/myapplication/Models/UserToken.java
+++ app/src/main/java/es/upm/miw/myapplication/Models/UserToken.java
@@ -1,61 +1,34 @@
 package es.upm.miw.myapplication.Models;
 
 
+import android.util.Base64;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+
+import es.upm.miw.myapplication.Utils.AppendingObjectOutputStream;
 
 public class  UserToken implements Serializable {
     private static final long serialVersionUID = 1L;
     private String token;
     private static final String USER_TOKEN = "data/data/es.upm.miw.myapplication/files/token.txt";
     public UserToken(String token) {
+        token = token.substring(token.toString().lastIndexOf("=") + 1);
+        token = token.substring(0,token.toString().length()-1);
         this.token = token;
     }
 
 
     public String getToken() {
         return token;
-    }
-
-
-    public static boolean isLogged(){
-        File yourFile = new File(USER_TOKEN);
-        try {
-            yourFile.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ArrayList<UserToken> token = new ArrayList<>();
-        FileInputStream fis = null;
-
-        try {
-            fis = new FileInputStream(USER_TOKEN);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        ObjectInputStream in = null;
-        try {
-            in = new ObjectInputStream(fis);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            while (true)
-                token.add((UserToken) in.readObject());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if(token.size()==1){
-            return true;
-        }else{
-            return false;
-        }
     }
 
     public static void destroyToken(){
@@ -95,6 +68,45 @@ public class  UserToken implements Serializable {
         }else{
             return null;
         }
+    }
+
+    public void saveUserToken(){
+        try
+        {
+            File file;
+            file = new File(USER_TOKEN);
+            file.delete();
+            FileOutputStream fos = new FileOutputStream(USER_TOKEN, true);
+
+            if (file.length() == 0) {
+                ObjectOutputStream out = new ObjectOutputStream(fos);
+                out.writeObject(this);
+                out.close();
+            } else {
+                ObjectOutputStream out = new AppendingObjectOutputStream(fos);
+                out.writeObject(this);
+                out.close();
+            }
+        }catch(IOException i)
+        {
+            i.printStackTrace();
+        }
+    }
+
+    public static String decoded(String JWTEncoded) throws Exception {
+        try {
+            String[] split = JWTEncoded.split("\\.");
+            int index = getJson(split[1]).indexOf("username") + 11;
+            int lastIndex = getJson(split[1]).indexOf(",") -14;
+            return getJson(split[1]).substring(index, index+lastIndex);
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
+    private static String getJson(String strEncoded) throws UnsupportedEncodingException{
+        byte[] decodedBytes = Base64.decode(strEncoded, Base64.URL_SAFE);
+        return new String(decodedBytes, "UTF-8");
     }
 
 }
